d7092ac6f949bbf650e19be72755a506981c2fda
AntonyRNS <antony.ryan.n.s@gmail.com>
2025-05-14 15:00:09 +0000

    v2.5(46)

--- models.py
+++ models.py
@@ -53,3 +53,19 @@ class Avaliacao(Base):
     nota = Column(Integer, nullable=False)
     comentario = Column(String(300))
     usuario = relationship('Usuario')
+
+
+class Autor(Base):
+    __tablename__ = 'autores' 
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(100), nullable=False)
+    nacionalidade = Column(String(100))
+    livros = relationship("Livro", back_populates="autor")
+
+class Livro(Base):
+    __tablename__ = 'livros'
+    id = Column(Integer, primary_key=True)
+    titulo = Column(String(100), nullable=False)
+    ano_publicacao = Column(DateTime, default=datetime.now)
+    autor_id = Column(Integer, ForeignKey('autores.id'))
+    autor = relationship("Autor", back_populates="livros")
